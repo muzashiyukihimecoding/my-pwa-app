737f51d18f6901d2990c040a8d694ee3bff0b791
Rasyid Nabil P <rasyidnabil06@gmail.com>
2026-06-22 10:56:48 +0700

    move; public icons

--- vite.config.js
+++ vite.config.js
@@ -21,13 +21,13 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "src/assets/atom192.png",
+            src: "public/atom192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "src/assets/atom512.png",
+            src: "public/atom512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
